08b71b03a5d7e417cc4a0a069d41339bd408c3bb
Pranta07 <palpranta@gmail.com>
2022-03-24 20:50:30 +0600

    necessary packages are installed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <p>Hello React Mama!</p>
+            <p>Hello Authors! You will be visible soon!!!</p>
         </div>
     );
 }
