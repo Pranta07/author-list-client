cb42103c2e9c1dc73a66a36c0e199185aed0a736
Pranta07 <palpranta@gmail.com>
2022-03-24 21:45:14 +0600

    menu options ui done

---
+++ src/Pages/Dashboard/SideDrawer/SideDrawer.js
@@ -0,0 +1,62 @@
+import { Logout, PeopleAlt } from "@mui/icons-material";
+import {
+    Divider,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Toolbar,
+} from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const menuItems = [
+    { title: "Author", to: "/author", icon: <PeopleAlt /> },
+    { title: "Favourite Author", to: "/favorite", icon: <PeopleAlt /> },
+    { title: "Search Author", to: "search", icon: <PeopleAlt /> },
+];
+
+const SideDrawer = (props) => {
+    const { handleSignOut } = props;
+
+    return (
+        <div>
+            <Toolbar />
+            <Divider />
+            <List>
+                {menuItems.map((item, index) => (
+                    <Link
+                        to={item.to}
+                        style={{
+                            textDecoration: "none",
+                            color: "blue",
+                        }}
+                    >
+                        <ListItem button key={index}>
+                            <ListItemIcon style={{ color: "lightblue" }}>
+                                {item.icon}
+                            </ListItemIcon>
+                            <ListItemText primary={item.title} />
+                        </ListItem>
+                    </Link>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                <ListItem button onClick={() => handleSignOut()}>
+                    <ListItemIcon
+                        style={{
+                            textDecoration: "none",
+                            color: "blue",
+                        }}
+                    >
+                        <Logout></Logout>
+                    </ListItemIcon>
+                    <ListItemText>Logout</ListItemText>
+                </ListItem>
+            </List>
+        </div>
+    );
+};
+
+export default SideDrawer;
