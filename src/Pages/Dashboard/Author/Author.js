99524f3a9cc5f0456eeee0d3a1bc38ea538ecf81
Pranta07 <palpranta@gmail.com>
2022-03-24 23:35:13 +0600

    loading progress added

--- src/Pages/Dashboard/Author/Author.js
+++ src/Pages/Dashboard/Author/Author.js
@@ -1,14 +1,24 @@
-import { Box, Grid, Typography, Tooltip, Divider } from "@mui/material";
+import {
+    Box,
+    Grid,
+    Typography,
+    Tooltip,
+    Divider,
+    LinearProgress,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import ListItemComponent from "../ListItemComponent/ListItemComponent";
 
 const Author = () => {
     const [allAuthor, setAllAuthor] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        setLoading(true);
         fetch("http://api.quotable.io/authors")
             .then((res) => res.json())
-            .then((data) => setAllAuthor(data.results));
+            .then((data) => setAllAuthor(data.results))
+            .finally(() => setLoading(false));
     }, []);
 
     // console.log(allAuthor);
@@ -27,21 +37,27 @@ const Author = () => {
             </Typography>
             <Divider></Divider>
 
-            <Box sx={{ padding: 3 }}>
-                <Grid
-                    container
-                    spacing={{ xs: 2, md: 3 }}
-                    columns={{ xs: 4, sm: 8, md: 12 }}
-                >
-                    {allAuthor.map((author) => (
-                        <Grid item xs={4} sm={4} md={6} key={author._id}>
-                            <ListItemComponent
-                                author={author}
-                            ></ListItemComponent>
-                        </Grid>
-                    ))}
-                </Grid>
-            </Box>
+            {loading ? (
+                <Box sx={{ width: "100%" }}>
+                    <LinearProgress />
+                </Box>
+            ) : (
+                <Box sx={{ padding: 3 }}>
+                    <Grid
+                        container
+                        spacing={{ xs: 2, md: 3 }}
+                        columns={{ xs: 4, sm: 8, md: 12 }}
+                    >
+                        {allAuthor.map((author) => (
+                            <Grid item xs={4} sm={4} md={6} key={author._id}>
+                                <ListItemComponent
+                                    author={author}
+                                ></ListItemComponent>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Box>
+            )}
         </>
     );
 };
